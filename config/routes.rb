75f7a5c99e262a409854e2f2ddf0505394823dc0
juan diego garcia <juandigt@hotmail.com>
2018-03-01 23:03:02 +0100

    cocktail App

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :cocktails do
+    resources :doses, only: [ :new, :create, :show]
+  end
+  resources :doses, only: [:destroy] #lo ponemos aqui por qeu no necesitamos el id de cocktail para eliminar una dose la ruta seria solo dose/id
 end
